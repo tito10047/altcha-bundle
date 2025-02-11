709193fc1a433b443b7acd67babb96e569b6fb64
tito10047 <jozef@mostka.sk>
2025-02-11 20:03:15 +0100

    added test for Validator

---
+++ tests/Validator/AltchaValidatorTest.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jozef Môstka
+ * Date: 11. 2. 2025
+ * Time: 19:43
+ */
+
+namespace Huluti\AltchaBundle\Tests\Validator;
+
+use AltchaOrg\Altcha\Algorithm;
+use AltchaOrg\Altcha\ChallengeOptions;
+use Huluti\AltchaBundle\Validator\Altcha;
+
+class AltchaValidatorTest extends \PHPUnit\Framework\TestCase
+{
+    public function testAltchaNotInRequest()
+    {
+        $requestStack = $this->createMock(\Symfony\Component\HttpFoundation\RequestStack::class);
+        $request = $this->createMock(\Symfony\Component\HttpFoundation\Request::class);
+        $requestStack->method('getCurrentRequest')->willReturn($request);
+        $request->request = $this->createMock(\Symfony\Component\HttpFoundation\ParameterBag::class);
+        $request->request->method('get')->willReturn(null);
+
+        $validator = new \Huluti\AltchaBundle\Validator\AltchaValidator(true, 'key', $requestStack);
+        $constraint = new Altcha();
+        $context = $this->createMock(\Symfony\Component\Validator\Context\ExecutionContextInterface::class);
+        $validator->initialize($context);
+
+        $context->expects($this->once())->method('buildViolation');
+
+        $validator->validate(null, $constraint);
+    }
+
+    public function testAltchaNotEncodedPropertly():void
+    {
+        $requestStack = $this->createMock(\Symfony\Component\HttpFoundation\RequestStack::class);
+        $request = $this->createMock(\Symfony\Component\HttpFoundation\Request::class);
+        $requestStack->method('getCurrentRequest')->willReturn($request);
+        $request->request = $this->createMock(\Symfony\Component\HttpFoundation\ParameterBag::class);
+        $request->request->method('get')->willReturn('not base64 encoded');
+
+        $validator = new \Huluti\AltchaBundle\Validator\AltchaValidator(true, 'key', $requestStack);
+        $constraint = new Altcha();
+        $context = $this->createMock(\Symfony\Component\Validator\Context\ExecutionContextInterface::class);
+        $validator->initialize($context);
+
+        $this->expectException(\JsonException::class);
+
+        $validator->validate(null, $constraint);
+
+    }
+
+    public function testAltchaNotValid():void
+    {
+        $requestStack = $this->createMock(\Symfony\Component\HttpFoundation\RequestStack::class);
+        $request = $this->createMock(\Symfony\Component\HttpFoundation\Request::class);
+        $requestStack->method('getCurrentRequest')->willReturn($request);
+        $request->request = $this->createMock(\Symfony\Component\HttpFoundation\ParameterBag::class);
+        $request->request->method('get')->willReturn(base64_encode(json_encode(['solution' => 'not valid'])));
+
+        $validator = new \Huluti\AltchaBundle\Validator\AltchaValidator(true, 'key', $requestStack);
+        $constraint = new Altcha();
+        $context = $this->createMock(\Symfony\Component\Validator\Context\ExecutionContextInterface::class);
+        $validator->initialize($context);
+
+        $context->expects($this->once())->method('buildViolation');
+
+        $validator->validate(null, $constraint);
+    }
+
+    public function testAltchaIsValid():void
+    {
+        $options = new ChallengeOptions([
+            'hmacKey' => 'test-key',
+            'maxNumber' => 100000,
+            'number' => 10,
+            'expires' => (new \DateTime())->modify("+1 day"),
+        ]);
+
+        $challenge = (array)\AltchaOrg\Altcha\Altcha::createChallenge($options);
+        $challenge['number'] = 10;
+
+
+        $requestStack = $this->createMock(\Symfony\Component\HttpFoundation\RequestStack::class);
+        $request = $this->createMock(\Symfony\Component\HttpFoundation\Request::class);
+        $requestStack->method('getCurrentRequest')->willReturn($request);
+        $request->request = $this->createMock(\Symfony\Component\HttpFoundation\ParameterBag::class);
+        $request->request->method('get')->willReturn(base64_encode(json_encode($challenge)));
+
+        $validator = new \Huluti\AltchaBundle\Validator\AltchaValidator(true, 'test-key', $requestStack);
+        $constraint = new Altcha();
+        $context = $this->createMock(\Symfony\Component\Validator\Context\ExecutionContextInterface::class);
+        $validator->initialize($context);
+
+        $context->expects($this->never())->method('buildViolation');
+
+        $validator->validate(null, $constraint);
+
+    }
+}
