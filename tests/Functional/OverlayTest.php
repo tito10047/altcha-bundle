254b4316a411a88353f70eb21ba38a32bc4c31b3
tito10047 <jozef@mostka.sk>
2025-10-24 13:15:17 +0200

    fix tests

--- tests/Functional/OverlayTest.php
+++ tests/Functional/OverlayTest.php
@@ -6,14 +6,15 @@ namespace Functional;
 use Tito10047\AltchaBundle\Tests\Functional\PantherTestCase;
 
 class OverlayTest  extends PantherTestCase {
-	public function testTwig(): void
+	public function testOverlay(): void
 	{
 		$client = static::createPantherOverlayClient();
 		$client->request('GET', '/');
 		$client->waitFor('.altcha-main',3);
 
 		$this->assertPageTitleContains('Welcome!');
-		$this->assertSelectorNotExists('.altcha-label');
+		$this->assertSelectorExists('.altcha-label');
+		$this->assertSelectorIsNotVisible('.altcha-label');
 
 		$this->assertSelectorExists("#script-altcha","Altcha script need be included");
 		$this->assertSelectorExists("#script-altcha-i18n","Altcha i18n script need be included");
