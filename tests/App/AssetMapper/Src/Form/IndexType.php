31f6d0e528f03a7ab8545bd60c28754e487b1b1c
tito10047 <jozef@mostka.sk>
2025-12-06 16:43:19 +0100

    add panther fixed branch for run tests

--- tests/App/AssetMapper/Src/Form/IndexType.php
+++ tests/App/AssetMapper/Src/Form/IndexType.php
@@ -11,7 +11,7 @@ use Symfony\Component\Form\FormView;
 
 class IndexType extends AbstractType {
 
-    public function buildForm(FormBuilderInterface $builder, array $options) {
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
         $builder->add("altcha",AltchaType::class);
     }
 
