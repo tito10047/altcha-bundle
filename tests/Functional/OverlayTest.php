cb964604d6af5a93727e7c4a0cb26837ffa17413
Jozef Mostka <jozef@mostka.sk>
2026-04-14 15:20:37 +0200

    env variable

--- tests/Functional/OverlayTest.php
+++ tests/Functional/OverlayTest.php
@@ -1,27 +1,29 @@
 <?php
 
-namespace Functional;
-
+namespace Tito10047\AltchaBundle\Tests\Functional;
 
 use Tito10047\AltchaBundle\Tests\Functional\PantherTestCase;
 
-class OverlayTest  extends PantherTestCase {
-	public function testOverlay(): void
-	{
-		$client = static::createPantherClient('Overlay');
-		$client->request('GET', '/');
-		$client->waitFor('.altcha-main',3);
+class OverlayTest extends PantherTestCase
+{
+    public function testOverlay(): void
+    {
+        $client = static::createPantherClient('Overlay');
+        $client->request('GET', '/');
+        $client->waitFor('.altcha-main', 3);
 
-		$this->assertPageTitleContains('Welcome!');
-		$this->assertSelectorExists('label[for^="altcha-checkbox-"]');
-		$client->submitForm("Submit");
-		$client->waitFor('.altcha-overlay-content',3);
-		$client->wait(1);
+        $this->assertPageTitleContains('Welcome!');
+        $this->assertSelectorExists('label[for^="altcha-checkbox-"]');
 
-		$this->assertSelectorTextContains('.altcha-overlay-content','Verifying you are human...');
+        $this->assertSelectorTextContains('#my-content', 'Verifying you are human...');
 
-		$this->assertSelectorExists("#script-altcha","Altcha script need be included");
-		$this->assertSelectorExists("#script-altcha-i18n","Altcha i18n script need be included");
+        $client->submitForm("Submit");
+        $client->waitFor('.altcha-overlay-content', 5);
+        $client->wait(1);
 
-	}
+        $this->assertSelectorTextContains('.altcha-overlay-content', 'Verifying you are human...');
+
+        $this->assertSelectorExists("#script-altcha", "Altcha script need be included");
+        $this->assertSelectorExists("#script-altcha-i18n", "Altcha i18n script need be included");
+    }
 }
